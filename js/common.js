bffdb19273dd049be82a4d2f9b6eab74f7e3a820
mak7sus <mak7sus@gmail.com>
2014-12-05 16:40:04 +0200

    no message

--- js/common.js
+++ js/common.js
@@ -5,3 +5,21 @@ head.ready(function() {
 	// });
 	console.log($('body').html());
 });
+
+function ShowHideSlider(){
+	var slider = document.getElementById("header__slider");
+	var block = document.getElementById("header__main");
+		
+	if (block.style.display == "none") {			
+		slider.style.display = "none";
+		block.style.display = "block";			
+	}
+	else {			
+		block.style.display = "none";
+		slider.style.display = "block";
+	}		
+}
+
+$('.slideshow').cycle({
+    	manualSpeed: 600,    
+});
